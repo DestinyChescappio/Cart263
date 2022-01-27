30931c169589589cd17c0415da03fe2d93c299ea
DestinyChescappio <chescappiodestiny@gmail.com>
2022-01-26 22:21:10 -0500

    E2: changed the array of animals into movie lines (riddles) - goal is to have the user get a display of wrong/right answer pages - create a title, 5 right answer pages, 1 losing page - give the user 3 chances before the losing page displays - !errors: to stop the answer and riddle from displaying simutaneously.

--- exercises/slanima-new-game-plus/js/script.js
+++ exercises/slanima-new-game-plus/js/script.js
@@ -9,145 +9,22 @@ author, and this description to match your project!
 "use strict";
 
 //array of animals
-const animals = [
-  "aardvark",
-  "alligator",
-  "alpaca",
-  "antelope",
-  "ape",
-  "armadillo",
-  "baboon",
-  "badger",
-  "bat",
-  "bear",
-  "beaver",
-  "bison",
-  "boar",
-  "buffalo",
-  "bull",
-  "camel",
-  "canary",
-  "capybara",
-  "cat",
-  "chameleon",
-  "cheetah",
-  "chimpanzee",
-  "chinchilla",
-  "chipmunk",
-  "cougar",
-  "cow",
-  "coyote",
-  "crocodile",
-  "crow",
-  "deer",
-  "dingo",
-  "dog",
-  "donkey",
-  "dromedary",
-  "elephant",
-  "elk",
-  "ewe",
-  "ferret",
-  "finch",
-  "fish",
-  "fox",
-  "frog",
-  "gazelle",
-  "gila monster",
-  "giraffe",
-  "gnu",
-  "goat",
-  "gopher",
-  "gorilla",
-  "grizzly bear",
-  "ground hog",
-  "guinea pig",
-  "hamster",
-  "hedgehog",
-  "hippopotamus",
-  "hog",
-  "horse",
-  "hyena",
-  "ibex",
-  "iguana",
-  "impala",
-  "jackal",
-  "jaguar",
-  "kangaroo",
-  "koala",
-  "lamb",
-  "lemur",
-  "leopard",
-  "lion",
-  "lizard",
-  "llama",
-  "lynx",
-  "mandrill",
-  "marmoset",
-  "mink",
-  "mole",
-  "mongoose",
-  "monkey",
-  "moose",
-  "mountain goat",
-  "mouse",
-  "mule",
-  "muskrat",
-  "mustang",
-  "mynah bird",
-  "newt",
-  "ocelot",
-  "opossum",
-  "orangutan",
-  "oryx",
-  "otter",
-  "ox",
-  "panda",
-  "panther",
-  "parakeet",
-  "parrot",
-  "pig",
-  "platypus",
-  "polar bear",
-  "porcupine",
-  "porpoise",
-  "prairie dog",
-  "puma",
-  "rabbit",
-  "raccoon",
-  "ram",
-  "rat",
-  "reindeer",
-  "reptile",
-  "rhinoceros",
-  "salamander",
-  "seal",
-  "sheep",
-  "shrew",
-  "silver fox",
-  "skunk",
-  "sloth",
-  "snake",
-  "squirrel",
-  "tapir",
-  "tiger",
-  "toad",
-  "turtle",
-  "walrus",
-  "warthog",
-  "weasel",
-  "whale",
-  "wildcat",
-  "wolf",
-  "wolverine",
-  "wombat",
-  "woodchuck",
-  "yak",
-  "zebra",
+const riddles = [
+  "The boy who lived", //Harry Potter
+  "We only wear Pink on Wednesdays", //Mean Girls
+  "I'll be back...", //The terminator
+  "Toto, I've got a feeling we're not in Kansas anymore", //the wizard of oz
+];
+
+const answers = [
+  "Harry Potter",
+  "Mean Girls",
+  "The Terminator",
+  "The Wizard of Oz",
 ];
 
 //setting a variable with an empty string
-let currentAnimal = ``;
+let currentRiddle = ``;
 //answer after the command
 let currentAnswer = ``;
 /**
@@ -158,7 +35,7 @@ function setup() {
   if (annyang) {
     //what the user says in order to trigger the responsivevoice
     let commands = {
-      "I think it is *animal": guessAnimal,
+      "*answer": guessMovie,
     };
     //to add the commands
     annyang.addCommands(commands);
@@ -176,7 +53,7 @@ Description of draw()
 */
 function draw() {
   //displaying the correct and wrong answer
-  if (currentAnswer === currentAnimal) {
+  if (currentAnswer === currentRiddle) {
     //correct answer
     fill(0, 255, 0);
   } else {
@@ -188,27 +65,25 @@ function draw() {
 
 //random animal said in reverse is triggered when mouse is pressed
 function mousePressed() {
-  currentAnimal = random(animals);
-  let reverseAnimal = reverseString(currentAnimal);
-  responsiveVoice.speak(reverseAnimal);
+  currentRiddle = random(riddles);
+  let movieRiddle = movieRiddleString(currentRiddle);
+  responsiveVoice.speak(movieRiddle);
 }
 
 //having the user guess the animal being called in reverse
-function guessAnimal(animal) {
+function guessMovie(answers) {
   //whatever was said by responsive voice
-  currentAnswer = animal.toLowerCase();
+  currentAnswer = answers.toLowerCase();
 }
 
 /**
 Reverses the provided string
 */
-function reverseString(string) {
+function movieRiddleString(string) {
   // Split the string into an array of characters
   let characters = string.split("");
-  // Reverse the array of characters
-  let reverseCharacters = characters.reverse();
   // Join the array of characters back into a string
-  let result = reverseCharacters.join("");
+  let result = characters.join("");
   // Return the result
   return result;
 }
