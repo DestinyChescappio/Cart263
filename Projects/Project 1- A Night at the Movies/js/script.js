aa060e3035711ae789e8f1b4200b79e8d3a323a1
DestinyChescappio <chescappiodestiny@gmail.com>
2022-02-15 21:48:39 -0500

    p1: included p5.play library

---
+++ Projects/Project 1- A Night at the Movies/js/script.js
@@ -0,0 +1,30 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  createSprite(400, 200, 50, 50);
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(255);
+  drawSprite();
+}
