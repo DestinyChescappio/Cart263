1acf635f9fdd550d35df29d820aaac64219f9c19
DestinyChescappio <chescappiodestiny@gmail.com>
2022-03-10 22:46:11 -0500

    Raving redactionist activity error: the top-secret class gets highlighted when clicked

--- activities/Raving-redactionist/js/script.js
+++ activities/Raving-redactionist/js/script.js
@@ -7,27 +7,49 @@ author, and this description to match your project!
 */
 
 "use strict";
+//to only re-redact in the top secrets' class if it's clicked
+$(`.top-secret`).on(`click`, redact);
 
+//every 5 seconds
+setInterval(revelation, 500);
 
+//to reverse redact if it's revealed
+function redact(event) {
+  //remove the revealed class
+  $(this).removeClass(`revealed`);
+  //then replace it with redacted
+  $(this).addClass(`redacted`);
+}
+
+//everything that's redacted, the program attempts to reveal by calling attemptReveal on ea. redacted object
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+//calling the attemptReveal on each object that will be redacted
+function attemptReveal() {
+  //variable r and assign with math.random
+  let r = Math.random();
+  //to check if r is than 0.1; to check how likely the hack is successful
+  if (r < 0.1) {
+    //if it got hacked
+    //remove the redacted class from (this)
+    $(this).removeClass(`redacted`);
+    // and add it to the reveal class
+    $(this).addClass(`revealed`);
+  }
+}
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
-function setup() {
-
-}
-
+function setup() {}
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
