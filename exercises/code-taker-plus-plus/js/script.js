33df179e0efb4c6671cabda8637b438632db1b17
DestinyChescappio <chescappiodestiny@gmail.com>
2022-03-17 12:55:56 -0400

    E7: started code taker

---
+++ exercises/code-taker-plus-plus/js/script.js
@@ -0,0 +1,46 @@
+/**
+Title of Project
+Author Name
+
+Da Vinci?!
+*/
+
+"use strict";
+//when the ser solves the answer
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "duh!": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+//the class found is when the mouse is over
+$(`.secret`).one(`mouseover`, function (event) {
+  //found only after 5 seconds
+  $(this).addClass(`found`, 500);
+  //drag when the mouse is over
+  $(`.secret`).draggable({
+    //options object 'helper' clones the word that is dragged- so they won't displace the texts
+    helper: `clone`,
+  });
+});
+
+//making the words droppable
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    //the letter is droppable
+    let letter = ui.draggable.text();
+    //adding the letter to the droppable area (the box)
+    $(this).append(letter);
+    //to stop it from being draggable the second time- can only drag a letter once
+    ui.draggable.draggable(`disable`);
+    //to stop the red higlight when found
+    ui.draggable.removeClass(`found`);
+    //check if the user got it right
+    if ($(this).text() === "Theremin") {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
