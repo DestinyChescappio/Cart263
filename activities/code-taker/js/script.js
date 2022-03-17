4bc2bbc86304c98b5e650674dfae3a9257593f62
DestinyChescappio <chescappiodestiny@gmail.com>
2022-03-17 01:47:25 -0400

    code taker activity: Highlighted words stay highlighted??? :(

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -2,10 +2,45 @@
 Title of Project
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Da Vinci?!
 */
 
 "use strict";
+//when the ser solves the answer
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "duh!": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
 
-// Code goes here
+//the class found is when the mouse is over
+$(`.secret`).one(`mouseover`, function (event) {
+  //found only after 5 seconds
+  $(this).addClass(`found`, 500);
+  //drag when the mouse is over
+  $(`.secret`).draggable({
+    //options object 'helper' clones the word that is dragged- so they won't displace the texts
+    helper: `clone`,
+  });
+});
+
+//making the words droppable
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    //the letter is droppable
+    let letter = ui.droppable.text();
+    //adding the letter to the droppable area (the box)
+    $(this).append(letter);
+    //to stop it from being draggable the second time- can only drag a letter once
+    ui.draggable.draggable(`disable`);
+    //to stop the red higlight when found
+    ui.draggable.removeClass(`found`);
+    //check if the user got it right
+    if ($(this).text() === "Theremin") {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
